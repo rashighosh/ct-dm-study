bff40b73d2d648df347862ed9c7e891468897870
rghosh96 <rashighosh96@gmail.com>
2026-07-18 21:27:51 -0400

    small tweaks

--- src/components/MainInteraction.jsx
+++ src/components/MainInteraction.jsx
@@ -1198,6 +1198,7 @@ export default function MainInteraction() {
         playGesture('lookright')
       } else {
         playGesture('stopAlexGesture')
+        playGesture('alexLookAtJordan')
       }
 
       setIsJordanWorkspaceOpen(true)
